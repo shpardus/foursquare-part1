a3eaaa0847f87aaba8f969abaf9dfa534bd7b7c2
Sabrina Pardus <shpardus@gmail.com>
2014-03-04 02:00:31 -0500

    no message

---
+++ Assets/scripts/Controller.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void FixedUpdate () {
+		// When 'W' key is pressed, goes straight
+		if ( Input.GetKey (KeyCode.W) || Input.GetKey (KeyCode.UpArrow) ) {
+			GetComponent<Rigidbody>().AddForce( GetComponent<Transform>().forward * 2f, ForceMode.VelocityChange );
+		}
+		else if ( Input.GetKey (KeyCode.S) || Input.GetKey (KeyCode.DownArrow) ) {
+			GetComponent<Rigidbody>().AddForce( GetComponent<Transform>().forward * -1, ForceMode.VelocityChange );
+		}
+		else {
+			GetComponent<Rigidbody>().velocity = Physics.gravity;
+		}
+		
+		// TURNING.
+		if ( Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.LeftArrow) ) {
+			transform.Rotate (new Vector3 (0f, -5f, 0f) );
+		}
+		else if ( Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.RightArrow) ) {
+			transform.Rotate (new Vector3 (0f, 5f, 0f) );
+		}
+
+	}
+}
---
+++ Assets/scripts/Controller2.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller2 : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate () {
+		// When 'T' key is pressed, goes straight
+		if ( Input.GetKey (KeyCode.T) || Input.GetKey (KeyCode.UpArrow) ) {
+			GetComponent<Rigidbody>().AddForce( GetComponent<Transform>().forward * 2f, ForceMode.VelocityChange );
+		}
+		else if ( Input.GetKey (KeyCode.G) || Input.GetKey (KeyCode.DownArrow) ) {
+			GetComponent<Rigidbody>().AddForce( GetComponent<Transform>().forward * -1, ForceMode.VelocityChange );
+		}
+		else {
+			GetComponent<Rigidbody>().velocity = Physics.gravity;
+		}
+		
+		// TURNING.
+		if ( Input.GetKey (KeyCode.F) || Input.GetKey (KeyCode.LeftArrow) ) {
+			transform.Rotate (new Vector3 (0f, -5f, 0f) );
+		}
+		else if ( Input.GetKey (KeyCode.H) || Input.GetKey (KeyCode.RightArrow) ) {
+			transform.Rotate (new Vector3 (0f, 5f, 0f) );
+		}
+
+	}
+}
